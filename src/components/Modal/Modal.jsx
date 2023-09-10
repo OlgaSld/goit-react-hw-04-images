80cf959984d955466805d6bdca26fabf9f30d154
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-09-11 00:35:07 +0300

    bugs with modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,26 +4,28 @@ import { Overlay, ModalWindow } from './Modal_styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export const Modal = (onModalClick, largeImage, alt, onBackDropClose) => {
+export const Modal = (onModalClick, largeImage, alt) => {
     useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
 
   const handleKeyDown = evt => {
     if (evt.code === 'Escape') {
      onModalClick();
     }
   };
+    
+    window.addEventListener('keydown', handleKeyDown);
+
+        
+    return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onModalClick])
 
   const onBackDropClose = evt => {
     if (evt.target === evt.currentTarget) {
       onModalClick();
     }
   };
-        
-    return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [onModalClick])
 
     return createPortal(
       <Overlay onClick={onBackDropClose}>
