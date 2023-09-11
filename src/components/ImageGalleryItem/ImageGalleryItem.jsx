9396c03c97bdfe43a401bf31c278b3a992796ef8
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-09-11 17:35:33 +0300

    final

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -13,20 +13,3 @@ export const ImageGalleryItem = ({image, onClick}) => {
       </GalleryItem>
     );
 }
-
-
-// import { GalleryItem, ImageGalleryItemImage } from './ImageGalleryItem_styled.jsx'
-
-// export const ImageGalleryItem = ({image, onClick}) => {
-//     return (
-//       <GalleryItem>
-//         <ImageGalleryItemImage
-//           src={image.webformatURL}
-//           alt={image.tag}
-//           onClick={() => onClick(image.largeImageURL, image.tag)}
-//         />
-//       </GalleryItem>
-//     );
-// }
-
-
